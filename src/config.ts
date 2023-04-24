6d4898466d3e87d988b4605a3702735694b46ec8
Vladyslav Biriuk <125121230+iVladd@users.noreply.github.com>
2023-04-24 16:12:18 +0300

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 import { Octokit } from "octokit";
 
 export const octokit = new Octokit({
-  auth: "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",
+  auth: "Your personal token",
 });
